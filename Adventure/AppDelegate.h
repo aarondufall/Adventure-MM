64775b95cc5bbe0d1909e080b9598e49c1200bcd
Aaron Dufall <aald212@gmail.com>
2014-03-11 09:13:10 -0700

    Initial Commit

---
+++ Adventure/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Adventure
+//
+//  Created by Aaron Dufall on 11/03/2014.
+//  Copyright (c) 2014 Aaron Dufall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
