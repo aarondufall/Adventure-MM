6156843c6b087fd5b61e02c731128cfc0ad76c13
Aaron Dufall <aald212@gmail.com>
2014-03-11 09:32:52 -0700

    As a user, I want to navigate the adventure

--- Adventure/ViewController.m
+++ Adventure/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *eatBoxButton;
+@property (weak, nonatomic) IBOutlet UIButton *cleanBoxButton;
 
 @end
 
@@ -25,5 +27,14 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(UIButton *)sender
+{
+    UIViewController *vc = segue.destinationViewController;
+    vc.title = sender.titleLabel.text;
+}
+
+
+
 
 @end
